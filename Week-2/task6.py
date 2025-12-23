8ea0b62b4c0e5fa82e940a19b0ec11e8d893d778
crypton1az <ernazar.niaz@gmail.com>
2025-12-23 22:22:15 +0500

    Update task6.py

--- Week-2/task6.py
+++ Week-2/task6.py
@@ -1,3 +1,13 @@
-def make_equal_length(string_list):
-    max_length_val = max(len(elem) for elem in string_list) if string_list else 0
-    return [elem + "_" * (max_length_val - len(elem)) for elem in string_list]
+def all_eq(string_list):
+    if not string_list:
+        return []
+    max_len_val = 0
+    for elem in string_list:
+        if len(elem) > max_len_val:
+            max_len_val = len(elem)
+    result_list = []
+    for elem in string_list:
+        if len(elem) < max_len_val:
+            elem = elem + "_" * (max_len_val - len(elem))
+        result_list.append(elem)
+    return result_list
