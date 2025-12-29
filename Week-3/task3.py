791e9bd7ad73fc976e9d10f119e90672a963c279
crypton1az <ernazar.niaz@gmail.com>
2025-12-29 17:19:03 +0500

    Update task3.py

--- Week-3/task3.py
+++ Week-3/task3.py
@@ -1,1 +1,29 @@
+1)
+a1 = float(input())
+b1 = float(input())
+a2 = float(input())
+b2 = float(input())
+
+h1 = (a1*a1 + b1*b1) ** 0.5
+h2 = (a2*a2 + b2*b2) ** 0.5
+
+if h1 > h2:
+    print("First is bigger")
+elif h1 < h2:
+    print("Second is bigger")
+else:
+    print("Equal")
+2)
+text = input()
+words = text.split()
+
+for w in words:
+    letters = list(w)
+
+    for i in range(len(letters)):
+        for j in range(i + 1, len(letters)):
+            if letters[i] > letters[j]:
+                letters[i], letters[j] = letters[j], letters[i]
+
+    print("".join(letters), end=" ")
 
